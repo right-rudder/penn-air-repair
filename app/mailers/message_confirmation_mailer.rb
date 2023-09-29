2c16aaea07013fcba92f06dabf07c051972cb0be
Tim Jedrek <tim@timjedrek.com>
2023-09-29 00:39:20 -0500

    move conditional to mailer itself instead of controller

--- app/mailers/message_confirmation_mailer.rb
+++ app/mailers/message_confirmation_mailer.rb
@@ -4,8 +4,12 @@ class MessageConfirmationMailer < ApplicationMailer
 
   def message_confirmation_email(message)
     @message = message
-
-    mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com", "info@pennairrepair.com"], reply_to: "info@pennairrepair.com", subject: "✈️ Thank you for contacting Penn Air Repair LLC")
+    if Rails.env.production?
+      mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com", "info@pennairrepair.com"], reply_to: "info@pennairrepair.com", subject: "✈️ Thank you for contacting Penn Air Repair LLC")
+    else
+      # Remove Client email when not in production
+      mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com"], reply_to: "info@pennairrepair.com", subject: "✈️ Thank you for contacting Penn Air Repair LLC")
+    end
   end
 
 end
