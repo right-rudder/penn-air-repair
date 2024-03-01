eda9440832461964500c8fb7ee9a5919ab96ae13
Tim Jedrek <tim@timjedrek.com>
2024-03-01 09:55:31 -0500

    add additional params to form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -99,6 +99,6 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:name, :email, :phone, :body, :make, :model, :n_number)
+      params.require(:message).permit(:name, :email, :phone, :body, :make, :model, :n_nummber)
     end
 end
